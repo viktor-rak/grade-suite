67cbf75f8ac8d9315059140f842ce9d1fe044391
viktor-rak <vrakov2012@gmail.com>
2025-05-10 23:32:59 -0400

    chore: got the page working

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express')
+const path = require('path')
 
+const app = express()
+const PORT = 3000;
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'templates'))
+
+
+app.use(express.static(path.join(__dirname, 'templates')))
+
+app.get('/', (req, res) => {
+	res.render('loginPage')
+})
+
+app.listen(PORT, () => {
+	console.log("Server running.")
+});
